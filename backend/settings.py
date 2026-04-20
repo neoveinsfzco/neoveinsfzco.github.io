38f380f549bc4ee7b6f0f624c61b2b8d97a1f9cd
Ahmad AlMulla <111180555+Ahmadmulla77@users.noreply.github.com>
2026-04-20 17:11:28 +0400

    fix try staticfiles

--- backend/settings.py
+++ backend/settings.py
@@ -167,8 +167,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# The URL to use when referring to static files
+STATIC_URL = '/static/'
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
